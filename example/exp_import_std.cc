0f055143b4dbcb68832f5fb06d1bb998cde50fda
lanmeng <pineconeforlanm@outlook.com>
2025-08-06 16:23:26 +0800

    feat: add utils module

--- example/exp_import_std.cc
+++ example/exp_import_std.cc
@@ -4,6 +4,16 @@
 import std;
 
 auto main() -> int {
+  try {
     std::println("Hello, this project is learn cxx!");
-    return 0;
+    const auto vec = std::vector<int>{1, 2, 3, 4, 5};
+    std::print("Vector elements: ");
+    for (const auto& elem : vec) {
+      std::print("{} ", elem);
+    }
+    std::println();
+  } catch (const std::exception& exp) {
+    std::cerr << exp.what() << '\n';
+  }
+  return 0;
 }
--- lcxx/boost/asio/server/sync_server.cc
+++ lcxx/boost/asio/server/sync_server.cc
@@ -1,9 +1,27 @@
 //
 // Created by lanm on 2025/8/6.
 //
+#include <cstdlib>
+import lcxx.utils;
 import std;
 
+namespace {
+auto sync_server_main() -> int {
+  std::println("Hello, sync server {{ version : {} }}...", SYNC_SERVER_VERSION);
+  std::println("use lcxx utils version: {}", lcxx::utils::version());
+  std::println("use boost version: {}", lcxx::utils::boost::lib_version());
+  return 0;
+}
+
+auto real_main() -> int { return sync_server_main(); }
+}  // namespace
+
 auto main() -> int {
-    std::println("Hello, this project is learn cxx!");
-    return 0;
+  try {
+    [[maybe_unused]] auto result = real_main();
+  } catch (const std::exception& exp) {
+    std::cerr << "Main Exception:" << exp.what() << '\n';
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
